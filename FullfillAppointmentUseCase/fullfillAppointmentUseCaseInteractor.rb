93793a9f85acea6d28d6a2ed44bcb5fccdbd7ef0
Renato Deris Prado <renatoprado21@gmail.com>
2018-03-20 22:53:39 -0300

    Learning how to deal with excpetions and fullfillappointmentusecase

--- FullfillAppointmentUseCase/fullfillAppointmentUseCaseInteractor.rb
+++ FullfillAppointmentUseCase/fullfillAppointmentUseCaseInteractor.rb
@@ -1,23 +1,33 @@
 require './Entities/appointment'
-class FullfillAppointmentUseCaseInteractor
 
+class FullfillAppointmentUseCaseModels
     ReqModel = Struct.new(:clientId, :coachId, :date)
-    ResModel = Struct.new(:clientId, :coachId, :date)
+    ResModel = Struct.new(:clientId, :coachId, :date) 
+end
 
+#class FullfillAppointmentDataErrors
+class CannnotFetchAppointment < StandardError
+end
+#end
+
+class FullfillAppointmentUseCaseInteractor
     def initialize(repository, interactorOutput)
         @repository = repository
         @interactorOutput = interactorOutput
     end
 
     def fullfillAppointment(reqModel)
-        @repository.fetchAppointment(reqModel.clientId, reqModel.coachId, reqModel.date) {
-            |clientId, coachId, appointment|
-            appointment.fullfill()
-            @repository.saveAppointment(appointment) {
-                resModel = ResModel.new(clientId, coachId, appointment.date)
-                @interactorOutput.presentFullfilledAppointment(resModel)
+        begin
+            @repository.fetchAppointment(reqModel.clientId, reqModel.coachId, reqModel.date) {
+                |clientId, coachId, appointment|
+                appointment.fullfill()
+                @repository.saveAppointment(appointment) {
+                    resModel = FullfillAppointmentUseCaseModels::ResModel.new(clientId, coachId, appointment.date)
+                    @interactorOutput.presentFullfilledAppointment(resModel)
+                }
             }
-        }
+        rescue CannnotFetchAppointment => e
+            @interactorOutput.presentCannotFetchAppointment(e.message)
+        end
     end
-
 end
--- FullfillAppointmentUseCase/fullfillAppointmentUseCaseInteractorTest.rb
+++ FullfillAppointmentUseCase/fullfillAppointmentUseCaseInteractorTest.rb
@@ -16,10 +16,20 @@ class RepoSpy
     end
 end
 
+class RepoSpyCantFetch < RepoSpy
+    def fetchAppointment(clientId, coachId, date)
+        raise(CannnotFetchAppointment, "Cannot fetch appointment for client #{clientId}, and coach #{coachId} on date #{date}")
+    end
+end
+
 class PresenterSpy
-    attr_accessor :presentFullfilledAppointmentCalled
+    attr_accessor :presentFullfilledAppointmentCalled, :presentCannotFetchAppointmentCalled
     def presentFullfilledAppointment(resModel)
         @presentFullfilledAppointmentCalled = true
+    end
+
+    def presentCannotFetchAppointment(message)
+        @presentCannotFetchAppointmentCalled = true
     end
 end
 
@@ -31,11 +41,25 @@ class CreateCoachUseCaseTest < Test::Unit::TestCase
 
         assert(!repo.saveAppointmentCalled)
         assert(!interactorOutput.presentFullfilledAppointmentCalled)
-
-        reqModel = FullfillAppointmentUseCaseInteractor::ReqModel.new("client@gmail.com", "coach@gmail.com", "date")
+        reqModel = FullfillAppointmentUseCaseModels::ReqModel.new("client@gmail.com", "coach@gmail.com", "date")
         interactorInput.fullfillAppointment(reqModel)
         assert(repo.saveAppointmentCalled)
         assert(interactorOutput.presentFullfilledAppointmentCalled)
 
     end
+
+    def testFullfillAppointmentShouldCallAppointErrorWhenFetchThrows
+        interactorOutput = PresenterSpy.new
+        repo = RepoSpyCantFetch.new 
+        interactorInput = FullfillAppointmentUseCaseInteractor.new(repo, interactorOutput)
+
+        assert(!repo.saveAppointmentCalled)
+        assert(!interactorOutput.presentFullfilledAppointmentCalled)
+        reqModel = FullfillAppointmentUseCaseModels::ReqModel.new("client@gmail.com", "coach@gmail.com", "date")
+        #assert_raise do
+        interactorInput.fullfillAppointment(reqModel)
+        assert(interactorOutput.presentCannotFetchAppointmentCalled)
+        #end
+    end
 end
+
